078bfd3003ccd3b27d73a0b2bedbfb5474a3b775
jwpythion <jaymesjohnson@me.com>
2018-08-08 10:01:38 -0700

    updated routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var path = require("path");
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -11,11 +13,11 @@ var PORT = process.env.PORT || 3000;
 
 // routes to index, static blog page, 
 // change rounts on actual html 
+app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-    res.render("index");
-
-});
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
 
 
 app.listen(PORT, function () {
